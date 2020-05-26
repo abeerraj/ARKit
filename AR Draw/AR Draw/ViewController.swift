b0b3ae8bd402e4eb56563cdf91269a3490a4fa18
Jacob Rozell <jrozell@myocv.com>
2020-05-26 17:07:02 -0500

    clean code

--- AR Draw/AR Draw/ViewController.swift
+++ AR Draw/AR Draw/ViewController.swift
@@ -10,16 +10,20 @@ import ARKit
 import UIKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-    @IBOutlet var sceneView: ARSCNView!
     
+    // IBOutlets
+    @IBOutlet var sceneView: ARSCNView!
     @IBOutlet var draw: UIButton!
+    
+    // ARConfig
     let config = ARWorldTrackingConfiguration()
     
+    // LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         sceneView.debugOptions = [.showWorldOrigin, .showFeaturePoints]
-        sceneView.showsStatistics = true
+        sceneView.showsStatistics = false
         sceneView.delegate = self
         sceneView.session.run(config)
     }
@@ -28,23 +32,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     public func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
         guard let pov = sceneView.pointOfView else { return }
         let transform = pov.transform
-        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)    // negate to get opposite direction
         let location = SCNVector3(transform.m41, transform.m42, transform.m43)
-        let frontOfCamera = orientation + location
+        let frontOfCamera = orientation + location                                      // This is a SCNVector3 of the front of camera
         
+        // Must throw this on the main thread to resolve access errors
         DispatchQueue.main.async {
+            
+            // If user is pressing button
             if self.draw.isHighlighted {
                 let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
                 sphereNode.position = frontOfCamera
                 sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
                 self.sceneView.scene.rootNode.addChildNode(sphereNode)
-            } else {
-                let pointer = SCNNode(geometry: SCNBox(width: 0.01, height: 0.01, length: 0.01, chamferRadius: 0.01/2))
+            }
+            
+            // User is not pressing button / Create pointer
+            else {
+                let pointer = SCNNode(geometry: SCNSphere(radius: 0.01))
+                pointer.name = "pointer"
                 pointer.position = frontOfCamera
                 pointer.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
                 
+                // remove any `pointer` nodes
                 self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
-                    if node.geometry is SCNBox {
+                    if node.name == "pointer" {
                         node.removeFromParentNode()
                     }
                 }
@@ -55,6 +67,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 }
 
+// Needed to compare two SCNVector3 objects
 func +(lhs: SCNVector3, rhs: SCNVector3) -> SCNVector3 {
     return SCNVector3Make(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z)
 }
