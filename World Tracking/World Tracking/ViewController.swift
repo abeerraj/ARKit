5870d0883eea792b62c19cb399f49f01cc5ad047
Jacob Rozell <jrozell@myocv.com>
2020-05-26 14:12:30 -0500

    started udemy

---
+++ World Tracking/World Tracking/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  World Tracking
+//
+//  Created by Jacob Rozell on 5/26/20.
+//  Copyright © 2020 jacobrozell. All rights reserved.
+//
+
+import ARKit
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Variables
+    @IBOutlet var sceneView: ARSCNView!
+    let arConfig = ARWorldTrackingConfiguration()
+    
+    // IBActions
+    @IBAction func addPressed(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0, 0, -0.3) // X, Y, Z
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    // ViewCycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        sceneView.session.run(arConfig)
+    }
+}
+
