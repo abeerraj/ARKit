2b41a79f818ad4096f5fae110d5f66790417f75e
Jacob Rozell <jrozell@myocv.com>
2020-05-26 16:55:30 -0500

    drawing successfully

--- AR Draw/AR Draw/ViewController.swift
+++ AR Draw/AR Draw/ViewController.swift
@@ -12,10 +12,7 @@ import UIKit
 class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     
-    @IBAction func buttonPressed(_ sender: Any) {
-        
-    }
-    
+    @IBOutlet var draw: UIButton!
     let config = ARWorldTrackingConfiguration()
     
     override func viewDidLoad() {
@@ -29,7 +26,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     // MARK: - ARSCNViewDelegate Methods
     public func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
-        print("rendering...")
+        guard let pov = sceneView.pointOfView else { return }
+        let transform = pov.transform
+        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let frontOfCamera = orientation + location
+        
+        DispatchQueue.main.async {
+            if self.draw.isHighlighted {
+                let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
+                sphereNode.position = frontOfCamera
+                sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+                self.sceneView.scene.rootNode.addChildNode(sphereNode)
+            } else {
+                let pointer = SCNNode(geometry: SCNBox(width: 0.01, height: 0.01, length: 0.01, chamferRadius: 0.01/2))
+                pointer.position = frontOfCamera
+                pointer.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+                
+                self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+                    if node.geometry is SCNBox {
+                        node.removeFromParentNode()
+                    }
+                }
+                
+                self.sceneView.scene.rootNode.addChildNode(pointer)
+            }
+        }
     }
+}
+
+func +(lhs: SCNVector3, rhs: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z)
 }
 
